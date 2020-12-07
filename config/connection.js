4b6ad3cbf53902cbf313ee23a5550974588a694b
Ibram Darwish <ibramdarwish@Ibrams-MacBook-Pro.local>
2020-12-06 19:27:24 -0500

    modify connection route

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -11,13 +12,14 @@ if (process.env.JAWSDB_URL) {
     password: "password",
     database: "burgers_db"
   });
+}
 
-  connection.connect(function (err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-  module.exports = connection;
+module.exports = connection;
